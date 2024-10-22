305b20eab8146c320525620e28edc78af6b6e830
Fredd <165827723+FreddieMaersk@users.noreply.github.com>
2024-10-22 14:43:42 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'Matemática',
+    'Qual a raíz quadrada de 25?',
+    '5.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Índia?',
+    'A capital da Índia é Nova Delhi.'
+)
+
+criaCartao(
+    'Música',
+    'Qual o nome do baterista da banda Queen?',
+    'Seu nome é Roger Taylor.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "Belos olhos" em Inglês?',
+    'Belos olhos em ingles é "Pretty eyes".'
+)
+
+criaCartao(
+    'Espanhol',
+    'Como se diz acima em espanhol?',
+    'Acima em espanhol é "ARRIBA".'
+)
+criaCartao(
+    'Música',
+    'Como se chama o primeiro álbum dos Beatles?',
+    'Ele se chama Please Please Me.'
+)
